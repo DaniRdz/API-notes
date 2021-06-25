b964c705d4d9142d0d127668c095002d7d59b74c
RdzCore <daniel_rod_con@hotmail.com>
2021-06-24 20:02:13 -0500

    added some GET and POST tests

--- tests/notes.test.js
+++ tests/notes.test.js
@@ -2,15 +2,92 @@ const supertest = require("supertest");
 const { app, server } = require("../index");
 
 const mongoose = require("mongoose");
+const Note = require("../models/Note");
 
 const api = supertest(app);
 
-test("notes are returned as json", async () => {
-  await api
-    .get("/api/notes")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+const initialNotes = [
+  {
+    content: "Hello bro :D",
+    important: true,
+    date: new Date(),
+  },
+  {
+    content: "This is a simul D:",
+    important: false,
+    date: new Date(),
+  },
+  {
+    content: "other note",
+    important: false,
+    date: new Date(),
+  },
+];
+
+beforeEach(async () => {
+  await Note.deleteMany({});
+
+  for (const note of initialNotes) {
+    const noteObject = new Note(note);
+    await noteObject.save();
+  }
 }, 11000);
+
+describe("Test GET endpoint API", () => {
+  test("notes are returned as json", async () => {
+    await api
+      .get("/api/notes")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("there are two notes", async () => {
+    const response = await api.get("/api/notes");
+    expect(response.body).toHaveLength(initialNotes.length);
+  });
+
+  test("The some note contain simul message", async () => {
+    const response = await api.get("/api/notes");
+
+    const contents = response.body.map((note) => note.content);
+    expect(contents).toContain("This is a simul D:");
+  });
+});
+
+describe("Test POST endpoint API", () => {
+  test("a valid note can be added", async () => {
+    const newNote = {
+      content: "this is a New note",
+      important: "true",
+    };
+    await api
+      .post("/api/notes/")
+      .send(newNote)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const response = await api.get("/api/notes");
+
+    const contents = response.body.map((note) => note.content);
+    expect(contents).toContain(newNote.content);
+    expect(response.body).toHaveLength(initialNotes.length + 1);
+  });
+
+  test("test without is not added", async () => {
+    const newNote = {
+      important: "true",
+    };
+    await api
+      .post("/api/notes/")
+      .send(newNote)
+      .expect(400)
+      .expect("Content-Type", /application\/json/);
+
+    const response = await api.get("/api/notes");
+
+    expect(response.body).toHaveLength(initialNotes.length);
+  });
+});
 
 afterAll(() => {
   server.close();
